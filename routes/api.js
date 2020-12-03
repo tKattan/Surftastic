359a74ec3a1fb2be23fe5c0d7c14c56327fb5e74
#19935252 <taleiven@gmail.com>
2020-12-03 19:26:13 +0100

    Added routes to backend

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const router = express.Router();
+const Surf = require('../models/surf');
+
+router.get('/surfs', (req, res, next) => {
+  Surf.find({}, 'place, date, no_other_people, rising_tide')
+  .then(data => res.json(data))
+  .catch(next)
+});
+
+router.post('/surfs', (req, res, next) => {
+    if(req.body.place || req.body.date){
+        Surf.create(req.body)
+          .then(data => res.json(data))
+          .catch(next)
+      }else {
+        res.json({
+          error: "The place or date fields are empty"
+        })
+      }
+});
+
+router.delete('/surfs/:id', (req, res, next) => {
+    Surf.findOneAndDelete({"_id": req.params.id})
+    .then(data => res.json(data))
+    .catch(next)
+})
+
+module.exports = router;
