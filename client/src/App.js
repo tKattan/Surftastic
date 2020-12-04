8dbb83b08304ed4925eeca82f02da5bc516efbc4
#19935252 <taleiven@gmail.com>
2020-12-04 01:48:11 +0100

    Correction in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import SiteBar from './component/sitebar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">  
+      <header className="App-header">
       </header>
     </div>
   );
